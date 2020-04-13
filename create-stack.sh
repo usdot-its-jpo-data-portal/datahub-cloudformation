80d693d715da92bfab7ca7cafdcd32b52d010e14
mvs5465 <mvs5465@gmail.com>
2020-04-13 11:54:55 -0400

    Update to use preferred deploy directive for cfn creation/updation

--- create-stack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-aws cloudformation create-stack --stack-name datahub-lambda-codebuild-ecr --template-body file://codebuild.template.yml
-# TODO - create this stack when things are finalized
-# aws cloudformation create-stack --stack-name datahub-sqs --template-body file://sqs.template.yml
--- update-stack.sh
+++ update-stack.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-aws cloudformation update-stack --stack-name datahub-lambda-codebuild-ecr --template-body file://codebuild.template.yml
-# TODO - create this stack when things are finalized
-# aws cloudformation update-stack --stack-name datahub-sqs --template-body file://sqs.template.yml
+aws cloudformation deploy --stack-name datahub-lambda-codebuild-ecr --template-body file://codebuild.template.yml
+aws cloudformation deploy --stack-name datahub-sqs --template-body file://sqs.template.yml
