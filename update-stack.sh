c254f43b4989a97d1ef0622e64329a63bc0072de
Matt S <mvs5465@gmail.com>
2020-04-14 10:14:27 -0400

    Add no-fail flag to prevent false errors

--- update-stack.sh
+++ update-stack.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-aws cloudformation deploy --stack-name datahub-lambda-codebuild-ecr --template-file codebuild-lambdas.template.yaml
-aws cloudformation deploy --stack-name datahub-ecs-codebuild-ecr --template-file codebuild-ecs.template.yaml
-aws cloudformation deploy --stack-name datahub-ecs-services --template-file ecs.template.yaml
-aws cloudformation deploy --stack-name datahub-sqs --template-file sqs.template.yaml
+aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-lambda-codebuild-ecr --template-file codebuild-lambdas.template.yaml
+aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-ecs-codebuild-ecr --template-file codebuild-ecs.template.yaml
+aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-ecs-services --template-file ecs.template.yaml
+aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-sqs --template-file sqs.template.yaml
