0b14ee9ca5ad7d43a8f10c2045214134dee64e29
mvs5465 <mvs5465@gmail.com>
2020-04-13 20:02:45 -0400

    Add sqs queues to deploy script

--- update-stack.sh
+++ update-stack.sh
@@ -2,3 +2,4 @@
 aws cloudformation deploy --stack-name datahub-lambda-codebuild-ecr --template-file codebuild-lambdas.template.yaml
 aws cloudformation deploy --stack-name datahub-ecs-codebuild-ecr --template-file codebuild-ecs.template.yaml
 aws cloudformation deploy --stack-name datahub-ecs-services --template-file ecs.template.yaml
+aws cloudformation deploy --stack-name datahub-sqs --template-file sqs.template.yaml
