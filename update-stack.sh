d213f0e8c9fc5558fd99089335844ae2bad2e250
chuehlien <chuehlien@gmail.com>
2021-07-21 16:27:36 -0400

    Comment out sections relating to canary/validation features

--- update-stack.sh
+++ update-stack.sh
@@ -2,4 +2,5 @@
 aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-lambda-codebuild-ecr --template-file codebuild-lambdas.template.yaml
 aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-ecs-codebuild-ecr --template-file codebuild-ecs.template.yaml
 aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-ecs-services --template-file ecs.template.yaml
-aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-sqs --template-file sqs.template.yaml
+# aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-sqs --template-file sqs.template.yaml
+
