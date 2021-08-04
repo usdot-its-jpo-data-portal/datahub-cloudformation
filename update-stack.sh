78b4f40ae2ffd84d0b63f2c89c9667f451310974
chuehlien <chuehlien@gmail.com>
2021-08-04 14:33:38 -0400

    Deploy sandbox-ecs-tasks stack

--- update-stack.sh
+++ update-stack.sh
@@ -3,4 +3,4 @@ aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-lamb
 aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-ecs-codebuild-ecr --template-file codebuild-ecs.template.yaml
 aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-ecs-services --template-file ecs.template.yaml
 # aws cloudformation deploy --no-fail-on-empty-changeset --stack-name datahub-sqs --template-file sqs.template.yaml
-
+aws cloudformation deploy --no-fail-on-empty-changeset --stack-name sandbox-ecs-tasks --template-file ecs-sandbox.template.yaml
